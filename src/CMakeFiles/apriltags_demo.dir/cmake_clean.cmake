c912339710cd72f189ad4c23409fe107fccc6356
JiangWenPL <JiangWenPL@users.noreply.github.com>
2018-07-11 18:33:31 +0800

    Init

---
+++ src/CMakeFiles/apriltags_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apriltags_demo.dir/apriltags_demo.cpp.o"
+  "libapriltags_demo.pdb"
+  "libapriltags_demo.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apriltags_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
